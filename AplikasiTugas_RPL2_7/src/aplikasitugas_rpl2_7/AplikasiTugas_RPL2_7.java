052b72964d3aa617dbd58cc8958ea23d1b76addd
helmynix <helmynix@yahoo.com>
2014-03-15 13:02:26 +0700

    versi 1
    tugas besar versi 1

---
+++ AplikasiTugas_RPL2_7/src/aplikasitugas_rpl2_7/AplikasiTugas_RPL2_7.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasitugas_rpl2_7;
+
+/**
+ *
+ * @author Helmy Faisal M <helmynix@gmail.com>
+ */
+public class AplikasiTugas_RPL2_7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
